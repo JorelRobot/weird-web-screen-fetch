2e12cb80074347f20d41a25603e624479628c494
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-04-18 22:33:07 -0500

    DB Conection Confing hecha

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const sql = require('mssql');
+const sqlconfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    server: 'localhost',
+    pool : {
+        max : 10,
+        min : 0,
+        idleTimeoutMillis : 30000
+    },
+    options : {
+        encrypt : true,
+        trustServerCertificate : true
+    }
+}
